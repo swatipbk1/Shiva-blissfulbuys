c61eeba10eca2909f188fdc9940ed83706778357
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-22 23:22:40 -0700

    c

--- src/Components/ShopSection.jsx
+++ src/Components/ShopSection.jsx
@@ -8,4 +8,13 @@ const ShopSection = () => {
       <ProductBox title="Health & Personal Care" image="box1.jpg" />
       <ProductBox title="Shop for valentines day" image="box2.jpg" />
       <ProductBox title="Luxury knits & bedding" image="box3.jpg" />
-   
+      <ProductBox title="Designer Clothing" image="box4.jpg" />
+      <ProductBox title="Electronics Devices" image="box5.jpg" />
+      <ProductBox title="Party Supplies" image="box6.jpg" />
+      <ProductBox title="Non-Fiction Content" image="box7.jpg" />
+      <ProductBox title="New Arrivals in Toys" image="box8.jpg" />
+    </div>
+  );
+}
+
+export default ShopSection;
