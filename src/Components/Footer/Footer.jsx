a17c6f49bfa013236d5acbe4b999885bc97ea22d
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-22 23:24:24 -0700

    c

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+// Footer.jsx
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-column">
+          <h3>Get to Know Us</h3>
+          <ul>
+            <li>Careers</li>
+            <li>BlissfulBuys Newsletter</li>
+            <li>About BlissfulBuys</li>
+            <li>Accessibility</li>
+            <li>Sustainability</li>
+            <li>Press Center</li>
+            <li>Investor Relations</li>
+           
