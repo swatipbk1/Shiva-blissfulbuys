4c042a31b4615f8e37489ea10eef8f693a5f5596
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-23 23:03:48 -0700

    c

---
+++ src/Components/ReviewOrderPage/ReviewOrderPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ReviewOrderPage() {
+  return (
+    <>
+      {/* Navbar */}
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        {/* Drawer */}
+        <button className="btn btn-outline-light" data-toggle="drawer" data-target="#amazonDrawer">
+          <i className="fas fa-sliders-h"></i>
+        </button>
+
+        {/* Navbar Brand */}
+        <a className="navbar-brand pl-2" href="navbar.html">
+          <img src="img/amazon_logo_white.png" height="30" width="100" alt="amazon logo white" />
+        </a>
+
+      
