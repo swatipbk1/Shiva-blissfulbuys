8e569da6bd6f175cb0cb5b9e2be7a5f25942c68a
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-23 23:06:30 -0700

    c

--- src/Components/ProductBox.jsx
+++ src/Components/ProductBox.jsx
@@ -3,4 +3,10 @@
 
 const ProductBox = ({ title, imageUrl }) => {
   return (
-   
+    <div className="product-box">
+      {/* Product box content */}
+    </div>
+  );
+}
+
+export default ProductBox;
