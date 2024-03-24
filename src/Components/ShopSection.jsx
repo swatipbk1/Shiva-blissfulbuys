6787e498b7618e4160f03b67188eb45e32c34e17
swatipbk1 <swati.pandey.ca@gmail.com>
2024-03-23 23:06:57 -0700

    c

---
+++ src/Components/ShopSection.jsx
@@ -0,0 +1,13 @@
+// ShopSection.js
+import React from 'react';
+import ProductBox from './ProductBox'; // Import ProductBox component
+
+const ShopSection = () => {
+  return (
+    <div className="shop-section">
+      <ProductBox title="Health & Personal Care" image="box1.jpg" />
+      <ProductBox title="Shop for valentines day" image="box2.jpg" />
+      <ProductBox title="Luxury knits & bedding" image="box3.jpg" />
+      <ProductBox title="Designer Clothing" image="box4.jpg" />
+      <ProductBox title="Electronics Devices" image="box5.jpg" />
+     
