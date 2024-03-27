7625b505b8b62e69cfe3059cd8cb770a62fa94d6
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-26 23:46:05 -0700

    Update Navbar.jsx

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -4,11 +4,7 @@ import { BsSearch, BsCart, BsGeoAlt } from "react-icons/bs";
 import { FaShoppingBag } from 'react-icons/fa';
 import './Navbar.css';
 
-          </select>
-          </div>
-        <
-          <FaShoppingBag className="blissfulbuys-icon" />
-          <span>BlissfulBuys</span>
+          
         </
   );
 }
