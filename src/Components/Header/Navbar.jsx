0b55392cf21365f6bfcff0192bb69179a5feba04
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-26 23:46:12 -0700

    Update Navbar.jsx

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,11 +1,6 @@
 
 import React from 'react';
-import { BsSearch, BsCart, BsGeoAlt } from "react-icons/bs";
-import { FaShoppingBag } from 'react-icons/fa';
-import './Navbar.css';
 
-          
-        </
   );
 }
 
