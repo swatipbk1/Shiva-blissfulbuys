30bb2a12f0ca455162a19b66d871c8ad0cb8243a
Swati Pandey <153130085+swatipbk1@users.noreply.github.com>
2024-03-26 23:48:49 -0700

    Update Navbar.jsx

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,16 +1,4 @@
 
 
 
-        <div className="nav-cart border">
-          <BsCart className="cart-icon" />
-          <span>Cart</span>
-        </div>
-        <div className="nav-blissfulbuys border">
-          <FaShoppingBag className="blissfulbuys-icon" />
-          <span>BlissfulBuys</span>
-     
-    </header>
-  );
-}
-
-export default Navbar;
+      
